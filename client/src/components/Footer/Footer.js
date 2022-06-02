99b7ca14e74dd24dbad42e95bc347049e55788c9
Jared Haralson <jharalson29@gmail.com>
2022-06-02 14:59:38 -0400

    Adding base for HomePage

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ export default function Footer(props) {
         <footer className="footer">
           <div className="footerInfo">
             <a href="tel:+4045558899" className="contact-info"><PhoneIcon className ="contactIcon" /></a>
-            <a href="mailto: jharalson29@gmail.com?subject=Art E-Commerce" className="contact-info"><EmailIcon className ="contactIcon" /></a>
+            <a href="mailto: jharalson29@gmail.com?subject=Touch of Spring" className="contact-info"><EmailIcon className ="contactIcon" /></a>
             <a href="https://github.com/jhara0994/MERN-E-Commerce/graphs/contributors" className="contact-info"><GitHubIcon className ="contactIcon"  /></a> 
           </div>                               
         </footer>
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,3 +1,13 @@
 import React from 'react'
+import CategoryMenu from '../../components/Category/CategoryMenu'
+import ProductList from '../../components/Product/ProductList'
+import ProductItem from '../../components/Product/ProductItem'
 
+function Home() {
+    return(
+        CategoryMenu,
+        ProductList,
+        ProductItem
+    )
+}
 
