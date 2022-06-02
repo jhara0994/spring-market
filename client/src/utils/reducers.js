9263cc21a07ef3764c7606f5ea7b9724aba26b52
Jared Haralson <jharalson29@gmail.com>
2022-06-01 23:22:57 -0400

    adding reducers to make front end work

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,1 +1,62 @@
 import { useReducer } from 'react';
+import {
+  UPDATE_PRODUCTS,
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  ADD_MULTIPLE_TO_CART,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  CLEAR_CART,
+  TOGGLE_CART,
+  SEARCH_BAR,
+  TOGGLE_DISPLAY_PRODUCT_IMAGE_UPLOAD
+} from './actions';
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+  
+      case ADD_TO_CART:
+        return {
+          ...state,
+          cartOpen: true,
+          cart: [...state.cart, action.product],
+        };
+  
+      case REMOVE_FROM_CART:
+        let newState = state.cart.filter((product) => {
+          return product._id !== action._id;
+        });
+  
+        return {
+          ...state,
+          cartOpen: newState.length > 0,
+          cart: newState,
+        };
+  
+      case CLEAR_CART:
+        return {
+          ...state,
+          cartOpen: false,
+          cart: [],
+        };
+  
+      case TOGGLE_CART:
+        return {
+          ...state,
+          cartOpen: !state.cartOpen,
+        };
+  
+      case UPDATE_CURRENT_CATEGORY:
+        return {
+          ...state,
+          currentCategory: action.currentCategory,
+        };
+
+      default:
+        return state;
+    }
+  };
+  
+  export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+  }
